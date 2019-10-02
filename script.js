02dd2cb0ca6fcbce3bdb10c3636fc76655b5d6ab
Anshul <malikanshul29@gmail.com>
2019-10-02 20:05:03 +0530

    prevent flicker when changing screenshot (#5)

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-
+const images = [
+  "/assets/screenshots/1.png",
+  "/assets/screenshots/2.png",
+  "/assets/screenshots/3.png",
+  "/assets/screenshots/3.png"
+];
 
 let i = 0;
 setInterval(() => {
-  i = ((i ) % 4) + 1
-  console.log(i, document.querySelector(`.demo :nth-child(${i})`))
-  document.querySelector(`.demo .show`).classList.remove('show')
-  document.querySelector(`.demo :nth-child(${i})`).classList.add('show')
-
-  // document.querySelector(`.demo`).appendChild(images[i])
-}, 2000)
+  i = (i + 1) % images.length;
+  document.querySelector('.demo img').src = images[i];
+}, 2000);
 
 
-    // var img=new Image();
+function preload() {
+  for (let i = 0; i < images.length; i++) {
+    let img = new Image(500, 500);
+    img.src = images[i];
+  }
+}
 
-  // var images = [];
-  // function preload() {
-  //     for (var i = 0; i < arguments.length; i++) {
-  //         images[i] = new Image(500, 500);
-  //         images[i].src = preload.arguments[i];
-  //     }
-  // }
-  //
-  // preload([
-  //   "/assets/screenshots/2.png",
-  //   "/assets/screenshots/3.png",
-  //   "/assets/screenshots/4.png"
-  // ])
+preload();
